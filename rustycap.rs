f1492063f318824b14dc72804d32e43909bbdd42
Gokul Das B <gokuldas.dev@gmail.com>
2014-06-12 22:34:00 +0530

    Removed hard coded path in favour of command line args

--- rustycap.rs
+++ rustycap.rs
@@ -2,12 +2,18 @@
 use pretty_hex::PrettyHex;
 use std::io::File;
 use std::path::Path;
+use std::os::args;
 
 mod pretty_hex;
 
 fn main() {
-    let mut file = File::open(&Path::new("dump.pcap"));
+    let argums = args();
+    if argums.len() != 2 {
+        println!("Command syntax: rustycap filename");
+        fail!("Invalid command syntax");
+    }
 
+    let mut file = File::open(&Path::new(argums.get(1).as_slice()));
     match file.read_to_end() {
         Ok(ref buf) => {
             let mut hexprint = PrettyHex::new();
