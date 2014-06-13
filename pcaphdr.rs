9c2149ee9c53b36078e02ede1771728f874c36a4
Gokul Das B <gokuldas.dev@gmail.com>
2014-06-13 14:21:21 +0530

    Global header: Added big-endian decoding, changed display

--- pcaphdr.rs
+++ pcaphdr.rs
@@ -1,5 +1,7 @@
 
-use std::io::{MemReader, IoResult};
+use std::io::{MemReader, IoResult, IoError, MismatchedFileTypeForOperation};
+
+enum PcapFileType { NormLE, NormBE, HResLE, HResBE}
 
 // PCAP Global Header
 pub struct PcapHdrS {
@@ -9,37 +11,55 @@ pub struct PcapHdrS {
     thiszone      : i32,  /* GMT to local correction */
     sigfigs       : u32,  /* accuracy of timestamps */
     snaplen       : u32,  /* max length of captured packets, in octets */
-    network       : u32   /* data link type */
+    network       : u32,  /* data link type */
+    file_type     : PcapFileType
 }
 
 impl PcapHdrS {
 
     pub fn new(reader: &mut MemReader) -> IoResult<PcapHdrS>{
-        Ok(PcapHdrS{
-            magic_number:  try!(reader.read_le_u32()),
-            version_major: try!(reader.read_le_u16()),
-            version_minor: try!(reader.read_le_u16()),
-            thiszone:      try!(reader.read_le_i32()),
-            sigfigs:       try!(reader.read_le_u32()),
-            snaplen:       try!(reader.read_le_u32()),
-            network:       try!(reader.read_le_u32())
-        })
+        let magic = try!(reader.read_le_u32());
+        match magic {
+            0xA1B2C3D4 => Ok(PcapHdrS{
+                magic_number:  0xA1B2C3D4,
+                version_major: try!(reader.read_le_u16()),
+                version_minor: try!(reader.read_le_u16()),
+                thiszone:      try!(reader.read_le_i32()),
+                sigfigs:       try!(reader.read_le_u32()),
+                snaplen:       try!(reader.read_le_u32()),
+                network:       try!(reader.read_le_u32()),
+                file_type:     NormLE
+            }),
+            0xD4C3B2A1 => Ok(PcapHdrS{
+                magic_number:  0xD4C3B2A1,
+                version_major: try!(reader.read_be_u16()),
+                version_minor: try!(reader.read_be_u16()),
+                thiszone:      try!(reader.read_be_i32()),
+                sigfigs:       try!(reader.read_be_u32()),
+                snaplen:       try!(reader.read_be_u32()),
+                network:       try!(reader.read_be_u32()),
+                file_type:     NormBE
+            }),
+            _ => Err(IoError{kind: MismatchedFileTypeForOperation,
+                             desc: "Wrong file type",
+                             detail: None })
+        }
     }
 
     pub fn display(&self) {
         println!("PCAP GLOBAL HEADER DETAILS");
         println!("Magic number   : {:X}", self.magic_number);
-        println!("Major version  : {:X}", self.version_major);
-        println!("Minor version  : {:X}", self.version_minor);
-        println!("Time zone      : {:X}", self.thiszone);
-        println!("Time precision : {:X}", self.sigfigs);
-        println!("Snap length    : {:X}", self.snaplen);
-        println!("Link type      : {:X}", self.network);
+        println!("Major version  : {  }", self.version_major);
+        println!("Minor version  : {  }", self.version_minor);
+        println!("Time zone(s)   : {  }", self.thiszone);
+        println!("Time precision : {  }", self.sigfigs);
+        println!("Snap length    : {  }", self.snaplen);
+        println!("Link type      : {  }", self.network);
     }
 
 }
 
 /*
 TODO:
-1. Handle BEnd & LEnd cases separately
+1. Validate BEnd case
 */
